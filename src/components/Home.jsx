df36524b9ce480b8268ab8e385a0a337553cdfa0
Lenon Fernandes <lenon.isidorio@logusinfo.com.br>
2025-04-04 16:48:38 -0300

    Projeto v0.9.9

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,23 @@
 import React, { useState } from "react";
+import styled from 'styled-components';
 import Timer from "./Timer"
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 2rem;
+    min-height: 100vh;
+    background: #f8f9fa;
+    `;
+
+    const Title = styled.h1`
+    font-size: 3rem;
+    color: #343a40;
+    margin-bottom: 1rem;
+
+    `
 
 const Home = () => {
 
@@ -17,13 +35,13 @@ const Home = () => {
         setIsRunning(!isRunning);
     }
     return (
-        <>
-        <h1>Home</h1>
+        <Container>
+        <Title>Home</Title>
         <Timer isRunning = {isRunning}/>
         {/* <button onClick={handleStart}>Start</button>
         <button onClick={handleStop}>Stop</button> */}
         <button onClick={toggleTimer}>{isRunning?'Stop' : 'Start'}</button>
-        </>
+        </Container>
     );
 }
 
